94a4e5e5ec7bb2f5ca44a7b2c615989256bec7ba
Jose Diaz <dzjose.root@gmail.com>
2020-11-11 00:37:22 -0500

    redirect root to login

--- apps/common/views.py
+++ apps/common/views.py
@@ -4,9 +4,11 @@ from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import *
 from django.urls import reverse_lazy
+from django.views.generic.base import RedirectView
 
-class HomeView(TemplateView):
-    template_name = 'common/login.html'
+class HomeView(RedirectView):
+    #template_name = 'common/login.html'
+    url = 'login/'
 
 class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = 'common/dashboard.html'
